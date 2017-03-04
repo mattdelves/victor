bf738f265987b957003ad4193f31bd81c3b4dd4d
Matthew Delves <matt@reformedsoftware.com>
2017-03-04 09:29:22 +0900

    Initial Commit

---
+++ Victor/Victor.h
@@ -0,0 +1,19 @@
+//
+//  Victor.h
+//  Victor
+//
+//  Created by Matthew Delves on 4/3/17.
+//  Copyright © 2017 Matthew Delves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Victor.
+FOUNDATION_EXPORT double VictorVersionNumber;
+
+//! Project version string for Victor.
+FOUNDATION_EXPORT const unsigned char VictorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Victor/PublicHeader.h>
+
+
